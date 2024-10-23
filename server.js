9c63f6a6feeee486ec26ee3432e0fca844327c82
Sergio <srg.a.neto@gmail.com>
2024-10-23 20:08:44 -0300

    Outra atualização, não aguento mais

--- server.js
+++ server.js
@@ -1,28 +1,43 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, 'publico', 'index.html');
+    const query = url.parse(req.url, true).query;
+    const num = parseInt(query.tabuada);
+    const seq = query.sequencia ? parseInt(query.sequencia) : 10;
 
-    // Verifica se a requisição é para a raiz
+    // Serve o arquivo HTML na raiz
     if (req.url === '/') {
-        fs.readFile(filePath, (err, data) => {
+        fs.readFile(path.join(__dirname, 'publico', 'index.html'), (err, data) => {
             if (err) {
-                res.writeHead(500);
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
                 res.end('Erro ao carregar o arquivo HTML');
             } else {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.end(data);
             }
         });
+    } else if (!isNaN(num)) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body>');
+        res.write(`<h1>Tabuada do ${num}</h1>`);
+        res.write('<ul>');
+        for (let i = 0; i <= seq; i++) {
+            res.write(`<li>${num} x ${i} = ${num * i}</li>`);
+        }
+        res.write('</ul>');
+        res.write('</body></html>');
+        res.end();
     } else {
-        res.writeHead(404);
-        res.end('404: Página não encontrada');
+        res.writeHead(400, { 'Content-Type': 'text/html' });
+        res.write('<h1>Erro: Número inválido</h1>');
+        res.end();
     }
 });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
