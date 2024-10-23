216572f76f62d04edaf62cdab4edbc3c57f52597
Sergio <srg.a.neto@gmail.com>
2024-10-23 20:30:09 -0300

    aaaaaaaaaa

--- server.js
+++ server.js
@@ -5,29 +5,37 @@ const path = require('path');
 
 const server = http.createServer((req, res) => {
     const query = url.parse(req.url, true).query;
+
+    // Pegando o número da tabuada e a sequência
     const num = parseInt(query.tabuada);
     const seq = query.sequencia ? parseInt(query.sequencia) : 10;
 
-    if (req.method === 'GET' && req.url.startsWith('/')) {
-        if (!isNaN(num)) {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<html><body>');
-            res.write(`<h1>Tabuada do ${num}</h1>`);
-            res.write('<ul>');
-            for (let i = 0; i <= seq; i++) {
-                res.write(`<li>${num} x ${i} = ${num * i}</li>`);
+    // Se a requisição for para a raiz, exibir o HTML
+    if (req.method === 'GET' && req.url === '/') {
+        fs.readFile(path.join(__dirname, 'publico', 'index.html'), 'utf-8', (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.write('<h1>Erro ao carregar a página</h1>');
+                res.end();
+                return;
             }
-            res.write('</ul>');
-            res.write('</body></html>');
-            res.end();
-        } else {
-            res.writeHead(400, { 'Content-Type': 'text/html' });
-            res.write('<h1>Erro: Número inválido</h1>');
-            res.end();
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else if (!isNaN(num)) { // Se o número for válido
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body>');
+        res.write(`<h1>Tabuada do ${num}</h1>`);
+        res.write('<ul>');
+        for (let i = 0; i <= seq; i++) {
+            res.write(`<li>${num} x ${i} = ${num * i}</li>`);
         }
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.write('<h1>Erro: Página não encontrada</h1>');
+        res.write('</ul>');
+        res.write('</body></html>');
+        res.end();
+    } else { // Caso contrário
+        res.writeHead(400, { 'Content-Type': 'text/html' });
+        res.write('<h1>Erro: Número inválido</h1>');
         res.end();
     }
 });
